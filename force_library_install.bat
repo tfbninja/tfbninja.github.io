11b2f3469d078d314cabef8df55d122b66bf07e7
Tim Barber <tfbninja@users.noreply.github.com>
2023-10-26 10:08:48 -0600

    parser getting better

---
+++ force_library_install.bat
@@ -0,0 +1,4 @@
+pip3.11 install pdfreader --force-reinstall -v --user
+pause
+pip3.11 install json --force-reinstall -v --user
+pip3.11 install re--force-reinstall -v --user
--- jsongen.py
+++ jsongen.py
@@ -3,6 +3,13 @@ from pdfreader import SimplePDFViewer
 import re
 
 non_alpha_regex = re.compile('[,\.!? ]')
+exception_list = ['A. C.,', 'A/C']
+
+def matches_exception(str1):
+    for ex in exception_list:
+        if ex in str1 and str1.index(ex) == 0:
+            return True
+    return False
 
 def compare_to(str1, str2):
     str1 = non_alpha_regex.sub('', str1)
@@ -10,11 +17,11 @@ def compare_to(str1, str2):
     for i in range(min(len(str1), len(str2))):
         diff = ord(str1[i]) - ord(str2[i])
         if diff != 0:
-            return diff / (10 ** i)
+            return (diff / 10.0 * 26) / (10 ** i)
 
     if len(str1) < len(str2):
         return -1
-    if len(str1) != len(str2):
+    if len(str1) > len(str2):
         return 1
     return 0
 
@@ -23,7 +30,7 @@ def acceptable_dist(str1, str2):
         return False
     if len(str2) == 0 and str1 == "A.":
         return True
-    if 0 < compare_to(str1, str2) < 2:
+    if 0 < compare_to(str1, str2) <= 1:
         return True
     if ord(str1[0]) - ord(str2[0]) == 1:
         return True;
@@ -44,29 +51,40 @@ b4_raw_file = open("pdfs/BlacksLaw4thRaw.txt", "r")
 b4_viewer.navigate(77) # page 77, first page of definitions
 
 # matches any number of capital letters with optional spaces in-between with a period at the end
-word_ptrn = re.compile("^\\b[A-Z .,]+[.,]{1,2}")
+word_ptrn = re.compile("^\\b[A-Z .,]+[.]{1,2}")
 current_word = ""
 buffer = ""
+exception_index = 0
 for canvas in b4_viewer:
     page_strings = canvas.strings
     page_text = canvas.text_content
     for string in page_strings:
+        is_word = False
         result = word_ptrn.match(string)
-        if result:
+        word = ""
+        end_pos = 0
+        if matches_exception(string):
+            is_word = True
+            word = exception_list[exception_index]
+            end_pos = len(word)
+            exception_index += 1
+        elif result:
             word = result.group(0)
+            end_pos = result.end()
             if acceptable_dist(word, current_word):
                 regex = "\\n\\(" + str(word).replace('.', "\\.") + " "
                 result2 = re.search(regex, page_text)
                 if result2:
-                    #print(buffer)
-                    #print(result2.group(0))
-                    print(word)
-                    b4_dict[current_word] = buffer
-                    buffer = ""
-                    current_word = word
-            #else:
-                #print('nope: "' + regex + '"')
-                #print(page_text)
+                    is_word = True
+        if is_word:
+            #print(compare_to(word, current_word))
+            is_word = True
+            #print(result2.group(0))
+            #print(word)
+            b4_dict[current_word] = buffer
+            print(current_word + ": " + b4_dict[current_word])
+            buffer = string[end_pos:]
+            current_word = word
         else:
             buffer += string + " "
 
