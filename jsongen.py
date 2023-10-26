3f91f28c69ac899002edc17ac87afae9d296411e
Tim Barber <tfbninja@users.noreply.github.com>
2023-10-26 10:48:16 -0600

    Update jsongen.py

--- jsongen.py
+++ jsongen.py
@@ -4,9 +4,11 @@ import re
 
 non_alpha_regex = re.compile('[,\.!? ]')
 exception_list = ['A. C.,', 'A/C']
+exception_index = 0
 
 def matches_exception(str1):
-    for ex in exception_list:
+    if exception_index < len(exception_list):
+        ex = exception_list[exception_index]
         if ex in str1 and str1.index(ex) == 0:
             return True
     return False
@@ -55,12 +57,19 @@ word_ptrn = re.compile("^\\b[A-Z .,]+[.]{1,2}")
 current_word = ""
 buffer = ""
 exception_index = 0
+pages_since_def = 0
+strings_since_def = []
 for canvas in b4_viewer:
     page_strings = canvas.strings
+    strings_since_def.append(page_strings)
     page_text = canvas.text_content
+    if pages_since_def > 20:
+        print("Unable to find definition after " + current_word)
+        print(strings_since_def)
+        print(page_text)
+        exit(1)
     for string in page_strings:
         is_word = False
-        result = word_ptrn.match(string)
         word = ""
         end_pos = 0
         if matches_exception(string):
@@ -68,25 +77,29 @@ for canvas in b4_viewer:
             word = exception_list[exception_index]
             end_pos = len(word)
             exception_index += 1
-        elif result:
-            word = result.group(0)
-            end_pos = result.end()
-            if acceptable_dist(word, current_word):
-                regex = "\\n\\(" + str(word).replace('.', "\\.") + " "
-                result2 = re.search(regex, page_text)
-                if result2:
-                    is_word = True
+        else:
+            result = word_ptrn.match(string)
+            if result:
+                word = result.group(0)
+                end_pos = result.end()
+                if acceptable_dist(word, current_word):
+                    regex = "\\n\\(" + str(word).replace('.', "\\.") + " "
+                    result2 = re.search(regex, page_text)
+                    if result2:
+                        is_word = True
         if is_word:
             #print(compare_to(word, current_word))
-            is_word = True
             #print(result2.group(0))
             #print(word)
             b4_dict[current_word] = buffer
             print(current_word + ": " + b4_dict[current_word])
             buffer = string[end_pos:]
             current_word = word
+            pages_since_def = 0
+            strings_since_def = [strings_since_def[len(strings_since_def) - 1]]
         else:
             buffer += string + " "
+    pages_since_def += 1
 
 
 b4_raw_file.close()
